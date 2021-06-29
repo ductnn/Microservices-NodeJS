3127c9b49ce843b47f2e18fb3218a57e30f450f6
ductnn <trannhuduc531998@gmail.com>
2021-06-29 22:23:53 +0700

    Initial Project

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config; 
+		
+const port = process.env.PORT || 3000; 
+const express = require('express');
+		
+// ROUTES
+		
+const app = new express();
+		
+// Connect Database
+		
+app.use(express.static('public'));
+		
+// MIDDLEWARE 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+		
+// VIEWS 
+app.set('view engine', 'pug'); 
+app.set('views', './views');
+		
+app.get('/', async (req, res) => { res.render('index', { name: 'Ductn' }); } );
+		
+app.listen(port, () => console.log('Server running on port ' + port));
+	
